95467d1c072899d2ec19e3536ed2b28cc018758b
Lili Boxer <ninaliboxer@gmail.com>
2019-07-10 16:02:06 -0700

    test set up for pathing

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,14 @@
+const root = document.getElementById('root');
+const colorList = document.createElement('ul');
+
 fetch('/api/v1/colors')
-  .then(res => res.join());
+  .then(res => res.json())
+  .then(colors => {
+    colors.forEach(color => {
+      const li = document.createElement('li');
+      li.textContent = `${color.name}`;
+      colorList.appendChild(li);
+    });
+  });
+
+root.appendChild(colorList);
--- test/main.test.js
+++ test/main.test.js
@@ -1,1 +1,10 @@
 // test pathing correctly 
+const request = require('supertest');
+
+describe('pathing', () => {
+  it('path / returns list of colors', () => {
+    return request(app)
+      .get('/')
+      .then(res => {})
+  })
+})
