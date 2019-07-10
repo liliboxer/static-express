759eec9607375538fe1be9006586b3aa239b7d4f
Lili Boxer <ninaliboxer@gmail.com>
2019-07-10 16:05:55 -0700

    test for / path is passing

--- test/main.test.js
+++ test/main.test.js
@@ -1,10 +1,14 @@
 // test pathing correctly 
 const request = require('supertest');
+const app = require('../lib/app');
 
 describe('pathing', () => {
   it('path / returns list of colors', () => {
     return request(app)
       .get('/')
-      .then(res => {})
-  })
+      .then(res => {
+        expect(res.text).toEqual(expect.stringContaining('Colors'));
+      });
+  });
 })
+;
